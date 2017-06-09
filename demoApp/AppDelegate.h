f0396179a13d10bd122a387985df2e3a288cd458
dineshpatil97 <dineshbhadane97@gmail.com>
2017-06-09 13:10:57 +0530

    Initial Commit

---
+++ demoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoApp
+//
+//  Created by Dinesh on 6/9/17.
+//  Copyright © 2017 Dinesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoApp
+//
+//  Created by Dinesh on 6/9/17.
+//  Copyright © 2017 Dinesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
